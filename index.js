7da26815a45e9b71e762519e0b7bac4316e8b9bc
Ahmedashraf <131619075+ahmedelzer@users.noreply.github.com>
2024-03-04 21:57:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,46 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
 const express = require("express");
-const http = require("http");
-const ws = require("ws");
-
+const morgan = require("morgan");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+// Create Express Server
+const Routes = require("./Routes.json");
 const app = express();
 
-// Define routes and their corresponding target URLs in JSON format
-const proxyConfig = {
-  "/car": { target: "https://dummyjson.com/products/1", changeOrigin: true },
-  "/images": { target: "http://images.example.com", changeOrigin: true },
-  "/ws": { target: "ws://websocket.example.com", ws: true }, // WebSocket route
-  // Add more routes as needed
-};
-
-// Create regular HTTP proxy middleware based on the configuration
-Object.entries(proxyConfig).forEach(([route, options]) => {
-  if (!options.ws) {
-    console.log("upgrade", route);
-    app.use(route, createProxyMiddleware(options));
-  }
+// Configuration
+const PORT = 3000;
+const HOST = "localhost";
+// Logging
+app.use(morgan("dev"));
+// Info GET endpoint
+app.get("/info", (req, res, next) => {
+  res.send(
+    "This is a proxy service which proxies to Billing and Account APIs."
+  );
 });
-app.use((req, res, next) => {
-  const route = req.url.split("?")[0]; // Extract the route from the request URL
-
-  // Check if the route should not be proxied
-  if (!proxyConfig[route]) {
-    // Handle the request here instead of proxying it
-    // For example, send a 404 response
-    res.status(404).send("Route not found");
-  } else {
-    // If the route should be proxied, continue to the next middleware
+// Authorization
+app.use("", (req, res, next) => {
+  if (req.headers.authorization) {
     next();
-  }
-});
-
-// Create WebSocket proxy server for WebSocket route
-const server = http.createServer(app);
-const wss = new ws.Server({ noServer: true });
-
-server.on("upgrade", (req, socket, head) => {
-  const route = Object.keys(proxyConfig).find((r) => req.url.startsWith(r));
-
-  const options = proxyConfig[route];
-  if (options && options.ws) {
-    wss.handleUpgrade(req, socket, head, (ws) => {
-      wss.emit("connection", ws, req);
-    });
   } else {
-    socket.destroy();
+    next();
+    console.log("req, res, next", res);
+
+    // res.sendStatus(403);
   }
 });
-
-wss.on("connection", (ws, req) => {
-  // You can handle WebSocket connections here
-  ws.on("message", (message) => {
-    console.log("Received message:", message);
-  });
+// Proxy endpoints
+Routes.map((route) => {
+  app.use(
+    route.route,
+    createProxyMiddleware({
+      target: route.target,
+      changeOrigin: route.changeOrigin,
+      pathRewrite: {
+        [`^${route.route}`]: "",
+      },
+    })
+  );
 });
-
-// Start the HTTP server on port 3000
-server.listen(3002, "localhost", () => {
-  console.log("Proxy server listening on port 3002");
+// Start the Proxy
+app.listen(PORT, HOST, () => {
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
 });
